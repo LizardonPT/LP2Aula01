92158f8113fe0d5126267b4793902a2eed57108c
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-09-27 11:52:08 +0100

    Cats

---
+++ CuteAnimal/Cat.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuteAnimal
+{
+    public class Cat
+    {
+        private string name;
+        private Mood mood;
+        private Feed feed;
+
+        public Cat(string name, Mood mood, Feed feed)
+        {
+            this.name = name;
+            this.mood = mood;
+            this.feed = feed;
+        }
+    }
+}
---
+++ CuteAnimal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CuteAnimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat("Randall", Mood.Grumpy, Feed.Starving);
+            Cat c2 = new Cat("JoJo", Mood.HyperActive, Feed.Full);
+        }
+    }
+}
