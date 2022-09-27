15d97af83dcb1169892be57870ff5e36ad31345e
LizardonPT <joaofilipegoncalves2000@gmail.com>
2022-09-27 12:21:31 +0100

    Print Cats

--- CuteAnimal/Cat.cs
+++ CuteAnimal/Cat.cs
@@ -8,14 +8,29 @@ namespace CuteAnimal
     public class Cat
     {
         private string name;
+        private int energy;
         private Mood mood;
         private Feed feed;
+        private Random rng;
 
-        public Cat(string name, Mood mood, Feed feed)
+        private Cat()
+        {
+            rng = new Random();
+            energy = rng.Next(101);
+        }
+        
+        public Cat(string name, Mood mood, Feed feed) : this()
         {
             this.name = name;
             this.mood = mood;
             this.feed = feed;
         }
+
+        public Cat(string name) : this(name, Mood.Happy, Feed.AboutToExplode)
+        {
+            
+        }
+
+        public string GetName() => name;
     }
 }
--- CuteAnimal/Program.cs
+++ CuteAnimal/Program.cs
@@ -8,6 +8,11 @@ namespace CuteAnimal
         {
             Cat c1 = new Cat("Randall", Mood.Grumpy, Feed.Starving);
             Cat c2 = new Cat("JoJo", Mood.HyperActive, Feed.Full);
+            Cat c3 = new Cat("Timon", Mood.IgnoringYou, Feed.Satisfied);
+
+            Console.WriteLine(c1.GetName());
+            Console.WriteLine(c2.GetName());
+            Console.WriteLine(c3.GetName());
         }
     }
 }
